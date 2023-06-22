7ef25d93b9183eb3d94c330ddcffd1450cc86ecf
Tivah <tivakudzezishiri@gmail.com>
2023-06-22 12:58:29 -0700

    from Tivah

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing to display
+  *@head: stack head
+  *@counter: line_number
+  *Return: nullify return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - printing to the upper part
+ * @head: stack head
+ * @counter: line_number
+ * Return: nullify return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
